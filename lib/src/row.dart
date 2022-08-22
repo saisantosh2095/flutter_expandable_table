3d7dbcd0914dbb2f27cc22793a28f152d05df412
Sai Santosh <sai99santosh@gmail.com>
2022-08-22 07:54:40 +0530

    reading isExpanded value from constructor in table row

--- lib/src/row.dart
+++ lib/src/row.dart
@@ -7,12 +7,12 @@ class ExpandableTableRow<T> {
   final Widget? legend;
   bool isExpanded = false;
 
-  ExpandableTableRow(
-      {Key? key,
-      required this.height,
-      required this.firstCell,
-      required this.children,
-      this.legend})
-      : assert(
-            children is List<Widget> || children is List<ExpandableTableRow>);
+  ExpandableTableRow({
+    Key? key,
+    required this.height,
+    required this.firstCell,
+    required this.children,
+    this.legend,
+    this.isExpanded = false,
+  }) : assert(children is List<Widget> || children is List<ExpandableTableRow>);
 }
